d024c553e813b660dffe9ab8f4da2962a0b5f824
Daniel Castillo <daniel.c.f.e@hotmail.com>
2014-03-09 23:16:45 -0430

    export pdf

--- application/views/reports/template.php
+++ application/views/reports/template.php
@@ -1,3 +1,16 @@
+<html>
+<head>
+	<link rel="stylesheet" href="http://localhost/proyecto/assets/css/reports/template.css">
+</head>
+<body>
+
+
 <div class="content">
-	<h1>Instituto Universitario Salesiano Padre Ojeda</h1>
+<?php if(isset($content)){
+	echo $content;
+} ?>
 </div>
+
+
+</body>
+</html>
